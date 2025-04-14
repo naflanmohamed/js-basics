be0aa27a9aa0ef3e111bb493b8f4dfd6b690fa71
naflanmohamed <naflanmohammed611@gmail.com>
2025-04-14 12:36:21 +0500

    js-basics

--- callbackFunction.js
+++ callbackFunction.js
@@ -1,8 +1,24 @@
-function x(y) {
-  console.log("I am x");
-  y();
+//callback function is
+
+// setTimeout(function () {
+//   console.log("I am setTimeout");
+// }, 5000);
+
+// function x(y) {
+//   console.log("I am x");
+//   y();
+// }
+
+// x(function y() {
+//   console.log("I am y");
+// });
+
+function attachEventListener() {
+  let count = 0;
+
+  document.getElementById("clickme").addEventListener("click", function xyz() {
+    console.log("button clicked", count++);
+  });
 }
 
-x(function y() {
-  console.log("I am y");
-});
+attachEventListener();
